11b4f5f07c76b0ce760e7b6ae43429a640303c78
Malachi Ashley <malachimda101@gmail.com>
2022-11-09 20:15:04 -0500

    Initialize react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
+
 const App = () => {
-  return <div className="text-lg text-blue-500">Hello world!</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<div>Home</div>} />
+      <Route path="test" element={<div>Test</div>} />
+      <Route path="about" element={<div>About</div>} />
+    </Routes>
+  );
 };
 
 export default App;
